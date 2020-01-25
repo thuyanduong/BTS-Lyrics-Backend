34d6076a14da4c48ae8562f3ad0016f8148520d0
github username <github email address>
2020-01-25 17:04:31 -0500

    slow debugging

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,19 +1,20 @@
 class AuthController < ApplicationController
   def create
     user = User.find_by(username: params[:username])
-    render json: "hello"
-    # if true #(user && user.authenticate(params["password"]) )
+    if (user && user.authenticate(params[:password]) )
     #   token = encode({user_id: user.id})
     #   data = user.user_data
     #   data[:success] = true
     #   data[:message] = "Successfully logged in",
     #   data[:token] = token
     #   render json: data, status: :accepted
-    # else
+      render json: "correct username and password"
+    else
     #   render json: {
     #     success: false,
     #     message: "Invalid username or password"
     #   }, status: :unauthorized
-    # end
+      render json: "inncorrect username or password"
+    end
   end
 end
